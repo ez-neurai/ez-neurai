bb3c93209b15cf1cc153307d2dfe95c2d32f0d9c
Brainary <62509122+ez-neurai@users.noreply.github.com>
2023-03-29 12:12:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import feedparser, datetime
 tistory_blog_uri="https://neurai.tistory.com/"
 feed = feedparser.parse(tistory_blog_uri+"/rss")
 
-markdown_text = """[![Typing SVG](https://readme-typing-svg.demolab.com?font=DM+Sans&duration=4000&pause=800&multiline=true&width=435&height=90&lines=Hi%2C+there.;Welcome+to+my+github+page!;Feel+free+to+look+around.)](https://git.io/typing-svg)
+markdown_text = """
 ## Recent blog posts
 """ # list of blog posts will be appended here
 
