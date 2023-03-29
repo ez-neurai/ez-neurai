9a973ef5b5519f36bf51fe21063ed1221b2fd2f7
Brainary <62509122+ez-neurai@users.noreply.github.com>
2023-03-29 13:19:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ lst = []
 
 for i in feed['entries']:
     dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    markdown_text = f"[{i['title']}]({i['link']}) - {dt}<br>\n"
     print(i['link'], i['title'])
 
 f = open("README.md",mode="w", encoding="utf-8")
