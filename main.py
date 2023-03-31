bc2925040f5d2d207265c70171d06aa47ca66a98
Brainary <62509122+ez-neurai@users.noreply.github.com>
2023-03-31 10:58:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ tistory_blog_uri="https://neurai.tistory.com/"
 feed = feedparser.parse(tistory_blog_uri+"/rss")
 
 markdown_text = """
+<span style = "font-family : AppleSDGothicNeoSB00; font-size : 4em;">
 [![Typing SVG](https://readme-typing-svg.demolab.com?font=DM+Sans&duration=4000&pause=800&multiline=true&width=435&height=90&lines=Hi%2C+there.;Welcome+to+my+github+page!;Feel+free+to+look+around.)](https://git.io/typing-svg)
 ## Recent blog posts
+</span>
 """ # list of blog posts will be appended here
 
 markdown_text1 = """
+<span style = "font-family : AppleSDGothicNeoSB00; font-size : 4em;">
 ## Contribution Wave
 [![Ashutosh's github activity graph](https://github-readme-activity-graph.cyclic.app/graph?username=ez-neurai&theme=nord)](https://github.com/ashutosh00710/github-readme-activity-graph)
+</span>
 """
 
 lst = []
