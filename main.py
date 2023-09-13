71dde6e13eee335e15a9204decceb7e2d6cc3c00
ez-neurai <ssh3543@gmail.com>
2023-09-13 18:31:20 +0900

    commit

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import feedparser, datetime
 
+# URI of the tistory blog
 tistory_blog_uri="https://neurai.tistory.com/"
 feed = feedparser.parse(tistory_blog_uri+"/rss")
 
-markdown_text = """
+# Markdown for profile image and typing SVG
+markdown_profile = """
 <img width="40%" src="https://github.com/ez-neurai/ez-neurai/assets/62509122/2361b392-ba8f-4edb-ae70-5320739c41a6"/>
-"""
-markdown_text1 = """
+<br>
 [![Typing SVG](https://readme-typing-svg.demolab.com?font=DM+Sans&duration=4000&pause=800&multiline=true&width=435&height=90&lines=Hi%2C+there.;Welcome+to+my+github+page!;Feel+free+to+look+around.)](https://git.io/typing-svg)
-## Recent blog posts
-""" # list of blog posts will be appended here
+"""
 
-lst = []
+# Markdown for blog posts
+markdown_blog_posts = "## Recent blog posts\n"
 
-
+# Append each blog post to markdown_blog_posts string
 for i in feed['entries']:
     dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
-    print(i['link'], i['title'])
+    markdown_blog_posts += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
 
-f = open("README.md",mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.write(markdown_text1)
-f.close()
+# Combine both markdowns
+full_markdown = markdown_profile + markdown_blog_posts
+
+# Write the markdown to README.md
+with open("README.md", "w", encoding="utf-8") as f:
+    f.write(full_markdown)
