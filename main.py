c98a9fd511c01ed286b90aec77c8c3127fff3da3
ez-neurai <ssh3543@gmail.com>
2023-09-13 18:59:28 +0900

    commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ tistory_blog_uri = "https://neurai.tistory.com/"
 feed = feedparser.parse(tistory_blog_uri + "/rss")
 
 # Image to be added at the top of README.md
-markdown_text = '<img width="40%" src="https://github.com/ez-neurai/ez-neurai/assets/62509122/2361b392-ba8f-4edb-ae70-5320739c41a6"/>\n\n'
+markdown_text = '<img width="40%" src="https://github.com/ez-neurai/ez-neurai/assets/62509122/2361b392-ba8f-4edb-ae70-5320739c41a6"/>\n\n<br>'
 
 markdown_text += '''
 [![Typing SVG](https://readme-typing-svg.demolab.com?font=DM+Sans&duration=4000&pause=800&multiline=true&width=435&height=90&lines=Hi%2C+there.;Welcome+to+my+github+page!;Feel+free+to+look+around.)](https://git.io/typing-svg)
@@ -13,7 +13,7 @@ markdown_text += '''
 
 for i in feed['entries']:
     dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
-    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\\n"
+    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>"
 
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
