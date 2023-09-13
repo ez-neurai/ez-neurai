8301d984e8ba496dd0ed7cdcf0269ea0998c607b
ez-neurai <ssh3543@gmail.com>
2023-09-13 18:27:40 +0900

    commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ feed = feedparser.parse(tistory_blog_uri+"/rss")
 
 markdown_text = """
 <img width="40%" src="https://github.com/ez-neurai/ez-neurai/assets/62509122/2361b392-ba8f-4edb-ae70-5320739c41a6"/>
+"""
+markdown_text1 = """
 [![Typing SVG](https://readme-typing-svg.demolab.com?font=DM+Sans&duration=4000&pause=800&multiline=true&width=435&height=90&lines=Hi%2C+there.;Welcome+to+my+github+page!;Feel+free+to+look+around.)](https://git.io/typing-svg)
 ## Recent blog posts
 """ # list of blog posts will be appended here
@@ -19,4 +21,5 @@ for i in feed['entries']:
 
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
+f.write(markdown_text1)
 f.close()
